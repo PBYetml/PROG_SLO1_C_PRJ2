9c8f9faa422ef4544e669840a4125aac830d70b2
Damien Bignens <damien.bignens@eduvaud.ch>
2023-04-04 12:21:19 +0200

    no message

--- DBS/PROJET1_S2_DAMIEN_BIGNENS/PROJET1_S2_DAMIEN_BIGNENs/PROJET1_S2_DAMIEN_BIGNENs/fonctionsPontDiv.c
+++ DBS/PROJET1_S2_DAMIEN_BIGNENS/PROJET1_S2_DAMIEN_BIGNENs/PROJET1_S2_DAMIEN_BIGNENs/fonctionsPontDiv.c
@@ -10,50 +10,75 @@
 //-- libraire (perso) --// 
 #include "PontResistifs.h"  
 
-void InitInfoPontDiviseur(double* pt_demoPontDiv)
+void InitInfoPontDiviseur(str_pontDiviseur* pt_demoPontDiv)
 {
-	char valeur;
+	int valeur;
 	printf("\n veuillez inserer le nombre de resistance possible dans votre pont diviseur:");
-	scanf_s("%c", &valeur);
+	scanf_s("%d", &valeur);
+	while ((getchar() != '\n') && (getchar() != EOF));
 	if (valeur > NB_MAX_resistance)
 	{
-		*pt_demoPontDiv = valeur; 
+		valeur = NB_MAX_resistance;
 	}
-	if (valeur <= 0)
+	else if (valeur <= 0)
 	{
-
-		valeur = 1; 
-	}
-	printf("\n veuillez inserer une valeur de tension");
-	scanf_s("%c", &valeur);
-	if (valeur > TENSION_MAX)
-	{
-		valeur = TENSION_MAX; 
-	}
-	if (valeur <= 1)
-	{
-
 		valeur = 1;
 	}
+	pt_demoPontDiv -> com_resistance = valeur;
+	int i;
+	for (i = 0; i < valeur; i++)
+	{
+		printf("Veuillez inserer une valeur de resistance allant de %1.1lf a %.0lfMohm pour R%d:", VAL_MIN_resistance, VAL_MAX_resistance / 10e6, i + 1);
+		scanf_s("%lf", &pt_demoPontDiv->tb_valeur_R[i]); 
+		while ((getchar() != '\n') && (getchar() != EOF));
+		if (pt_demoPontDiv->tb_valeur_R[i] < VAL_MIN_resistance || pt_demoPontDiv->tb_valeur_R[i] > VAL_MAX_resistance)
+		{
+			printf("!!! Erreur de valeur !!! \n");
+			i--;				 
+		}
+	}
 
-	pt_demoPontDiv >> 1 = valeur; 
+	printf("Veuillez inserer la tension d'alimentation du montage :");
+	scanf_s("%d", &valeur);
+	while ((getchar() != '\n') && (getchar() != EOF));
+
+	if (valeur > TENSION_MAX)
+	{
+		valeur = TENSION_MAX;
+	}
+	else if (valeur < 1)
+	{
+		valeur = 1;
+	}
+	pt_demoPontDiv->alime = valeur; //Stock la valeur dans la variable tensionAlimVe
 }
 
-void CalculTensionPontDivisueur(struct str_ParamA* pt_ParamA)
-{
-	int valeurR2;
-	int valeurR1;
-	int V_pont;
-	int valeur_optonue; 
-	valeurR2  = pt_ParamA->com_resistance * pt_ParamA->tb_valeur;
-	valeurR1 = pt_ParamA->com_resistance  *pt_ParamA->tb_valeur;
-	valeur_optonue = V_pont * (valeurR2 / (valeurR2 + valeurR1));
 
+
+void CalculTensionPontDivisueur(str_pontDiviseur* pt_demoPontDiv)
+{
+	double sommeResistances = 0;  
+	int i;
+	for (i = 0; i < pt_demoPontDiv->com_resistance; i++)
+	{
+		sommeResistances = pt_demoPontDiv->tb_valeur_R[i] + sommeResistances;
+	}
+
+	int e;  //Index
+	double tensionResistance_n; //variable pour VpontRu
+	for (e = 0; e < pt_demoPontDiv->com_resistance; e++)
+	{
+		tensionResistance_n = pt_demoPontDiv->alime * (pt_demoPontDiv->tb_valeur_R[e] / sommeResistances);		//tensionResistanceU = VpontRu
+		pt_demoPontDiv->tb_bornes_resistance[e] = tensionResistance_n;
+	}
 }
 
-void AffichageTensionPontDivisueur(int demoPontDiv)
+
+void AffichageTensionPontDivisueur(str_pontDiviseur pt_demoPontDiv)
 {
-	printf("\n la valeur entre chaque resistance %f3 ", demoPontDiv);
-
-
+	int i;
+	for (i = 0; i < pt_demoPontDiv.com_resistance; i++)
+	{
+		printf("Tension sur la resistance R%d vaut %1.3lf[V]\n", i + 1, pt_demoPontDiv.tb_bornes_resistance[i]);
+	}
 }
