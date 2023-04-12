88f83adf0f5607df1b5e305074c732aafcffac62
JeremyETMLES <113518025+JeremyETMLES@users.noreply.github.com>
2023-04-12 18:34:45 +0200

    Add files via upload
    Correction de la sur-définition et changement définition RMAX pour une notation scientifique

--- JAR/PROJET1_S2_AFFOLTER/PROJET1_S2_AFFOLTER/PontResistifs.h
+++ JAR/PROJET1_S2_AFFOLTER/PROJET1_S2_AFFOLTER/PontResistifs.h
@@ -19,12 +19,10 @@
 // Variables globales
 #define NBMAXRES	10			// Nombre maximum de résistances
 #define	VCCMAX		30			// Tension maximum du pont
-#define RMAX		10000000.0	// Valeur maximum d'une résistnace
+#define RMAX		1e7			// Valeur maximum d'une résistnace
 #define	RMIN		0.1			// Valeur minimum d'une résistance
 
 // Stuctures
-#ifndef STRUCTURE_H
-#define	STRUCTURE_H
 typedef struct
 {
 	char nbRes;		// Nombre de résistances dans le pont
@@ -32,7 +30,6 @@ typedef struct
 	float	tb_ValRes[10];	// Tableau comprenant la valeur des résistances
 	float	tb_URes[10];	// Tableau comprenant la tension sur chaque résistance
 }pontDiviseur;
-#endif // ! STRUCTURE_H
 
 
 // Prototype de fonction
