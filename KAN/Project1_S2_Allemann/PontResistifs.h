55e4bb4cae9d2d26622e9e840cd20928d3091cce
Kevin-AllemannETML-ES <113518392+Kevin-AllemannETML-ES@users.noreply.github.com>
2023-05-10 21:41:01 +0200

    Update PontResistifs.h

--- KAN/Project1_S2_Allemann/PontResistifs.h
+++ KAN/Project1_S2_Allemann/PontResistifs.h
@@ -2,15 +2,15 @@
 *	Nom du projet:		Project_S2_Allemann
 *	Nom du fichier:		PontResistifs.h
 *	Auteur:				Kevin Allemann
-*	Date de création:	07.03.2022
+*	Date de crÃ©ation:	07.03.2022
 *
-*	Description:		en tête de mainProject1
+*	Description:		en tÃªte de mainProject1
 */
 
 #ifndef ENTETE_MES_FONCTIONS
 #define ENTETE_MES_FONCTIONS
 
-#define MAX_NOMBRE_RESISTANCES 10
+const MAX_NOMBRE_RESISTANCES 10
 #define TENSION_MAX_PONT_DIVISEUR 30
 #define VALEUR_MAX_RESISTANCE 10e6 // 10*10^6
 #define VALEUR_MIN_RESISTANCE 0.1
